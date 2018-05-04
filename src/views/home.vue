6bab73620bf3987778a86d1bb2abb85e6204bc57
bo <yangbo@incarcar.com>
2018-05-04 19:23:44 +0800

    'init'

---
+++ src/views/home.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        9999999999
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "home",
+        data() {
+            //数据
+            return {}
+        },
+        methods: {
+            //方法
+        },
+        mounted() {
+            //生命周期，实例化，并挂载到dom
+        },
+        components: {
+            //注册子组件
+        },
+        props: {
+            //接收父组件的传值
+        },
+        watch: {
+            //监听数据的改变
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/map.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+
+<script>
+
+    export default {
+        name: "map",
+        data() {
+            //数据
+            return {}
+        },
+        methods: {
+            //方法
+        },
+        mounted() {
+            //生命周期，实例化，并挂载到dom
+        },
+        components: {
+            //注册子组件
+        },
+        props: {
+            //接收父组件的传值
+        },
+        watch: {
+            //监听数据的改变
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
